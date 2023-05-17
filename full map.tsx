2f7efd823145fc2958087909380e52032c1d3ea1
Mathis Debuire <68806646+64ix@users.noreply.github.com>
2023-05-17 10:14:24 +0200

    test script

---
+++ full map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="full map" tilewidth="32" tileheight="32" tilecount="10000" columns="100">
+ <image source="../../../Downloads/Group 1.png" width="3200" height="3200"/>
+</tileset>
---
+++ zones.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="zones" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="tilesets/WA_Special_Zones.png" width="192" height="64"/>
+ <tile id="6">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
