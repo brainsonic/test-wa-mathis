f6e2243c9c62926d93587c75c5febdfd70dab964
Korhonen <david.korhonen@brainsonic.com>
2023-06-13 15:45:47 +0200

    Liens tileset

--- tilesets/bird.tsx
+++ tilesets/bird.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="bird" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="../../../../Downloads/MinifolksForestAnimals/Outline/MiniBird.png" width="64" height="48"/>
+ <image source="MiniBird.png" width="64" height="48"/>
 </tileset>
