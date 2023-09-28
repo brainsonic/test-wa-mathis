9ee4f00d596a137e3c500f115d444239e06b3126
Cindy-Politi <cindy.politi@brainsonic.com>
2023-09-28 17:00:57 +0200

    Fix bug embed

--- Nom zone Labo yumi.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Nom zone Labo yumi" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="tilesets/Labo Yumi.png" width="320" height="320"/>
-</tileset>
