57e461052a2df96d73bf63f07a68cedbf9f7009c
Paul G <pguan19@gmail.com>
2023-11-02 16:08:09 +0100

    fix: deleted tileset

--- tilesets/chat-zone.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="chat-zone" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="chat-zone.png" width="163" height="96"/>
-</tileset>
