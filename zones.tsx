824df1c3c7112f09779f9336d8c28af0d1c54e61
Paul G <pguan19@gmail.com>
2024-01-23 12:11:59 +0100

    feat: Map2

--- zones.tsx
+++ zones.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="zones" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+<tileset version="1.10" tiledversion="1.10.2" name="zones" tilewidth="32" tileheight="32" tilecount="12" columns="6">
  <image source="tilesets/WA_Special_Zones.png" width="192" height="64"/>
+ <tile id="2">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="6">
   <properties>
    <property name="collides" type="bool" value="true"/>
