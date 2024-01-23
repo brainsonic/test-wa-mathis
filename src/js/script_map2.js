7b472aa2e143dba59808b3f2d20fb4d2d56055b3
Paul G <pguan19@gmail.com>
2024-01-23 17:17:03 +0100

    test: Auto-TP

--- src/js/script_map2.js
+++ src/js/script_map2.js
@@ -2,4 +2,9 @@ import {} from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 
 WA.onInit().then(() => {
     console.log('TEST SUCCESS');
+    console.log("Indice : ", WA.player.state["cafetHint"]);
+    if (WA.player.state["cafetHint"] == null)
+    {
+        WA.nav.goToRoom('/map.tmj');
+    }
 });
