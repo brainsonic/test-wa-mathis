4f6f8300801faef7366202b0ec7f384a35b85f66
Misterfacile <92047026+Misterfacile@users.noreply.github.com>
2024-01-23 17:04:52 +0000

    Deploying to gh-pages from master @ d05cfc5338d741390b1bb10dc561125bafab04b3 🚀

---
+++ assets/script_map2-28115030.js
@@ -0,0 +1,1 @@
+import"https://unpkg.com/@workadventure/scripting-api-extra@^1";WA.onInit().then(()=>{console.log("TEST SUCCESS"),console.log("Indice : ",WA.player.state.cafetHint),WA.player.state.cafetHint==null&&WA.nav.goToRoom("/@/tcm/workadventure/map.tmj")});
