ec3cbe9955270bb97a8fee842dbf8b4de7ddc1a7
Paul G <pguan19@gmail.com>
2024-01-24 13:29:58 +0100

    feat: URL tp

--- src/js/script_map2.js
+++ src/js/script_map2.js
@@ -3,8 +3,13 @@ import {} from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 WA.onInit().then(() => {
     console.log('TEST SUCCESS');
     console.log("Indice : ", WA.player.state["cafetHint"]);
+    let url_segments =  window.location.href.split('/');
+
+    console.log(url_segments);
+    url_segments[url_segments.length - 1] = "map.tmj";
+
     if (WA.player.state["cafetHint"] == null)
     {
-        WA.nav.goToRoom("/@/tcm/workadventure/map.tmj");
+        WA.nav.goToRoom(url_segments.join('/'));
     }
 });
